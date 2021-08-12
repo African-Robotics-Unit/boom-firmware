c8521632615e57b7c93174aad5c84f07eb4ede06
Chris Mailer <christophermailer@icloud.com>
2021-08-12 18:30:02 +0200

    Add encoder PLL estimator

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 Encoder pitch(1, 0);
 Encoder yaw(11, 10);
 
-IntervalTimer speedTimer;
+IntervalTimer pllTimer;
 
 const byte pitchIndexPin = 2;
 const byte ledPin = 13;
@@ -15,27 +15,28 @@ volatile bool pitchIndexFound = false;
 const float yawRadius = 2.558; // pivot to end mounting distance [m]
 const float pitchRadius = 2.475; // pivot to pivot distance [m]
 const unsigned int gearRatio = 4.0;
-const unsigned int cpr = 4000; // encoder counts per revolution
-const unsigned int sampleFrequency = 1000; //Hz
-const unsigned int speedUpdateFrequency = 20; //Hz higher frequency -> higher minimum speed
+const unsigned int CPR = 4000; // encoder counts per revolution
+const unsigned int serialFreq = 1000; // [Hz]
 const unsigned int laptopBaud = 1000000;
 
+// PLL estimator stuff
+// https://discourse.odriverobotics.com/t/rotor-encoder-pll-and-velocity/224
+const unsigned int pllFreq = 10000; // [Hz] not sure what this should be
+const unsigned int pllBandwidth = 1000; // [rad/s] ODrive default
+const float pll_kp = 2.0 * pllBandwidth;
+const float pll_ki = 0.25 * (pll_kp * pll_kp); // critically damped
+float pitch_pos_estimate = 0; // [counts]
+float pitch_vel_estimate = 0; // [counts/s]
+float yaw_pos_estimate = 0; // [counts]
+float yaw_vel_estimate = 0; // [counts/s]
+
 unsigned long t = 0;
-
-int prevYawValue = 0;
-int prevPitchValue = 0;
-
-float x = 0; // boom end horizontal position [m]
-float y = 0; // boom end vertical position [m]
-float dx = 0; // boom end horizontal speed [m/s]
-float dy = 0; // boom end vertical speed [m/s]
 
 // function declarations
 void send(float);
 void pitchIndexInterrupt();
-float countsToRadians(int);
-void calculatePosition();
-void calculateSpeed();
+float countsToRadians(float);
+void pllLoop();
 
 
 void setup() {
@@ -44,24 +45,27 @@ void setup() {
   pinMode(pitchIndexPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(pitchIndexPin), pitchIndexInterrupt, FALLING);
 
-  speedTimer.begin(calculateSpeed, 1E6 / speedUpdateFrequency);
-
   Serial.flush();
 	Serial.begin(laptopBaud);
 	Serial.write("Teensy comms initiated");
 
   yaw.write(0);
   while (!pitchIndexFound);
+  pllTimer.begin(pllLoop, 1E6 / pllFreq);
   t = micros();
 }
 
 // all serial communication must be done inside the main loop
 void loop() {
-  t += 1E6 / sampleFrequency;
+  t += 1E6 / serialFreq;
   // send header
 	Serial.write((uint8_t)0xAA);
 	Serial.write((uint8_t)0x55);
-  calculatePosition();
+  // calculate boom end pos and vel
+  float x = countsToRadians(yaw_pos_estimate) * yawRadius; // boom end horizontal position [m]
+  float y = countsToRadians(pitch_pos_estimate) * pitchRadius; // boom end vertical position [m]
+  float dx = countsToRadians(yaw_vel_estimate) * yawRadius; // boom end horizontal speed [m/s]
+  float dy = countsToRadians(pitch_vel_estimate) * pitchRadius; // boom end vertical speed [m/s]
   // send data
   send(x);
   send(y);
@@ -70,6 +74,7 @@ void loop() {
   // wait to ensure fixed sample frequency
   while(t > micros());
 }
+
 
 // Sends as sequential bytes
 void send(float data) {
@@ -85,19 +90,23 @@ void pitchIndexInterrupt() {
 }
 
 // converts encoder counts to an angle in radians
-float countsToRadians(int counts) {
-  return (float(counts) / float(cpr * gearRatio)) * 2.0*PI;
+float countsToRadians(float counts) {
+  return (counts / (float)(CPR * gearRatio)) * 2.0f*PI;
 }
 
-void calculatePosition() {
-  x = countsToRadians(yaw.read()) * yawRadius;
-  y = countsToRadians(pitch.read()) * pitchRadius;
-}
-
-void calculateSpeed() {
-  //position difference over time
-  dx = countsToRadians(yaw.read() - prevYawValue) * yawRadius * speedUpdateFrequency;
-  dy = countsToRadians(pitch.read() - prevPitchValue) * pitchRadius * speedUpdateFrequency;
-  prevYawValue = yaw.read();
-  prevPitchValue = pitch.read();
+// PLL loop
+// Runs at frequency defined by 'pllFreq'
+void pllLoop() {
+  float pll_period = 1.0 / pllFreq;
+  // predicted current pos
+  pitch_pos_estimate += pll_period * pitch_vel_estimate;
+  yaw_pos_estimate += pll_period * yaw_vel_estimate;
+  // discrete phase detector
+  float pitch_delta_pos = (float)(pitch.read() - (int32_t)floor(pitch_pos_estimate));
+  float yaw_delta_pos = (float)(yaw.read() - (int32_t)floor(yaw_pos_estimate));
+  // pll feedback
+  pitch_pos_estimate += pll_period * pll_kp * pitch_delta_pos;
+  pitch_vel_estimate += pll_period * pll_ki * pitch_delta_pos;
+  yaw_pos_estimate += pll_period * pll_kp * yaw_delta_pos;
+  yaw_vel_estimate += pll_period * pll_ki * yaw_delta_pos;
 }
